6b282fbb2f5244d32210b4fc289cb8c54ffdecb9
Yanxin Luo <yanxinluo28@gmail.com>
2026-03-26 22:20:02 -0400

    Initial Commit

---
+++ SyncSensorApp/SyncSensorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncSensorAppApp.swift
+//  SyncSensorApp
+//
+//  Created by Yanxin Luo on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SyncSensorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyncSensorAppTests/SyncSensorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyncSensorAppTests.swift
+//  SyncSensorAppTests
+//
+//  Created by Yanxin Luo on 3/26/26.
+//
+
+import Testing
+@testable import SyncSensorApp
+
+struct SyncSensorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
